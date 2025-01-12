202a8e2d143c4d4e80598368d75bc94d1c4d6e01
Abdou <2000abdallah2014@gmail.com>
2025-01-12 14:37:51 +0100

    created slug page to populate project page dynamically

---
+++ pages/projects/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+    <span>project slug is: {{ $route.params.slug }}</span>
+  </template>
+  
+  <script setup>
+  import {projects} from "~/data/data"
+  
+  const route = useRoute()
+  console.log(route.params) // { id: '123' }
+  const urlSlug =  route.params.slug
+const project = computed(()=>projects.find(obj=> obj.slug === urlSlug))
+
+  </script>
