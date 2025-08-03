35061593e91f4619a65cf42515f19ae8bf5fef54
Abdou <2000abdallah2014@gmail.com>
2025-08-03 10:58:41 +0100

    minor loader spacing fix

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="loader-cont">
-        <p>"Patience is not simply the ability to wait, it's how we behave while we're waiting." <br> Joyce Meyer</p>
+        <p>"Patience is not simply the ability to wait, it's how we behave while we're waiting." <br> <span class="block mt-2">Joyce Meyer</span></p>
         <div id="loader"></div>
     </div>
 </template>
