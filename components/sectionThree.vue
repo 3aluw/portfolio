5ceb1d97e9f9bf3734c7bbe466e8031830110621
Abdou <2000abdallah2014@gmail.com>
2025-08-04 09:42:22 +0100

    minor styling improvements for section 3

--- components/sectionThree.vue
+++ components/sectionThree.vue
@@ -11,7 +11,7 @@
                 <h1 class="hidden sm:block sm:text-6xl">ABOUT ME</h1>
                 <h2 class="text-xl sm:text-2xl accent-text self-center sm:self-auto">Abdellah Hoauche | Vue developer
                 </h2>
-                <p class="leading-relaxed pt-4"> I'm a web developer with a strong preference for working on<strong>
+                <p class="leading-relaxed pt-2 sm:pt-4"> I'm a web developer with a strong preference for working on<strong>
                         Nuxt/Vue projects</strong>. I'm
                     always looking to
                     contribute to <strong>open-source projects</strong>, learn new things, and collaborate with
@@ -21,14 +21,13 @@
                     currently open
                     to job opportunities or internships where I can grow and make an impact.<br>
 
-                    I'm also very curious about the <strong>Web3</strong> space and excited about its potential. When
-                    I'm not coding,
-                    you'll probably find me watching or playing football, or trying (and usually failing) to get my
-                    super lazy cat to do anything productive 😺.<br>
-
-                    <strong class="text-lg text-accent">Let’s build something cool together!</strong>
+                    I'm also very curious about the <strong>Web3</strong> space and excited about its potential. 
+                   <span class="hidden sm:inline"> WhenI'm not coding, you'll probably find me watching or playing football, or trying (and usually failing) to get my
+                    super lazy cat to do anything productive 😺.</span>
+                    <br>
+                    <strong class="text-lg text-accent pt-2">Let’s build something cool together!</strong>
                 </p>
-                <div class="links pt-4 flex gap-16 items-center">
+                <div class="links pt-2 sm:pt-4 flex gap-16 items-center">
                     <NuxtLink to="https://www.linkedin.com/in/abdellah-houache-479256185/" class="link" target="_blank"
                         rel="noopener">
                         <div>
@@ -79,7 +78,7 @@ onMounted(() => {
 @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Fira+Mono&display=swap');
 
 .s-three-main {
-    max-width: min(1200px,100vw);
+    max-width: min(1200px, 100vw);
     margin: 0 auto;
     overflow: hidden;
 
