f8902c3f108e29d34b5458a1bc561f0aab5ff126
Abdou <2000abdallah2014@gmail.com>
2025-08-04 09:32:34 +0100

    refactor: section 3 animation

--- components/sectionThree.vue
+++ components/sectionThree.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="my-8" >
-        <div   
+    <div class="my-8">
+        <div
             class="s-three-main sm:!mx-8 flex flex-col sm:flex-row justify-between items-center gap-8 !px-4 sm:!px-20 ">
 
             <div class="pic-cont">
@@ -29,14 +29,14 @@
                     <strong class="text-lg text-accent">Let’s build something cool together!</strong>
                 </p>
                 <div class="links pt-4 flex gap-16 items-center">
-                    <NuxtLink  to="https://www.linkedin.com/in/abdellah-houache-479256185/" class="link" target="_blank"
+                    <NuxtLink to="https://www.linkedin.com/in/abdellah-houache-479256185/" class="link" target="_blank"
                         rel="noopener">
                         <div>
                             <img class="w-14" src="~/assets/logos/linkedin.svg" alt="">
                         </div>
                     </NuxtLink>
                     <NuxtLink class="link" to="mailto:2000abdallah2014@gmail.com" target="_blank" rel="noopener">
-                        <div><img  class="w-10 logo" src="~/assets/logos/mail.svg" alt=""></div>
+                        <div><img class="w-10 logo" src="~/assets/logos/mail.svg" alt=""></div>
                     </NuxtLink>
                     <NuxtLink class="link" to="https://twitter.com/3aluwa" target="_blank" rel="noopener">
                         <div><img class="w-10" src="~/assets/logos/twitter.svg" alt=""></div>
@@ -56,27 +56,22 @@
 <script setup lang="ts">
 
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-onMounted(()=>{
-    gsap.from('.link',{
-    x : '100vw',
-    duration:2.5,
-    markers: true,
-    stagger: {
-        amount:1,
-        from: "center",
-        ease:"elastic.out"
-    },
-    scrollTrigger: ".link",
-    
-})
-
-let tl = gsap.timeline({
-    scrollTrigger:".s-three-main"
-});
-tl.to(".circle-border", {duration: 2, rotate: -45,ease:"power2.in"})
-.to(".circle-border", {duration: 5, rotate: 360,ease:"power1.out"})
+onMounted(() => {
+    let tl = gsap.timeline({
+        scrollTrigger: ".s-three-main"
+    });
+    tl.from('.link', {
+        x: '100vw',
+        duration: 4,
+        stagger: {
+            amount: 1,
+            from: "center",
+            ease: "power1.out"
+        }
+    }, 0)
+        .to(".circle-border", { duration: 1.5, rotate: -45, ease: "power2.in" }, 0)
+        .to(".circle-border", { duration: 4.5, rotate: 360, ease: "power1.out" }, ">")
 })
 </script>
 
@@ -84,17 +79,17 @@ tl.to(".circle-border", {duration: 2, rotate: -45,ease:"power2.in"})
 @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Fira+Mono&display=swap');
 
 .s-three-main {
-    max-width: 1200px;
+    max-width: min(1200px,100vw);
     margin: 0 auto;
-
+    overflow: hidden;
 
 }
 
 .pic-cont {
-    position: relative; /* so ::after is positioned relative to this container */
+    position: relative;
+    /* so ::after is positioned relative to this container */
     clip-path: circle(50% at 50% 50%);
-    background: #2B2146 
-    
+    background: #4C3A72
 }
 
 .pic-cont img {
@@ -105,13 +100,13 @@ tl.to(".circle-border", {duration: 2, rotate: -45,ease:"power2.in"})
 
 .pic-cont .circle-border {
     position: absolute;
-    top: 50%;  
+    top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%); 
+    transform: translate(-50%, -50%);
     width: calc(100% - 5px);
     aspect-ratio: 1/1;
-    border: 5px var(--green-accent);
-    border-style:  dashed double none;
+    border: 5px #E5E9F0;
+    border-style: dashed double none;
     border-radius: 50%;
     box-sizing: content-box;
 }
